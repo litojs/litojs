e50b58ca7e11497b692842f92f7a92607f54f450
Indra Zulfi Mushoddaq <me@indrazm.com>
2024-11-13 20:50:52 +0700

    fixing build scripts

--- bin/build.ts
+++ bin/build.ts
@@ -1,7 +1,18 @@
 import { $ } from "bun";
+import { build } from "tsup";
 
 await $`rm -rf dist`;
-await $`bun build src/**/*.ts --target=bun`;
-// await $`tsc`;
+
+await Promise.all([
+    build({
+        splitting: false,
+        entryPoints: ["src/**/*.ts"],
+        format: ["esm"],
+        outDir: "dist",
+        minify: true,
+        sourcemap: false,
+        dts: true,
+    }),
+]);
 
 process.exit();
