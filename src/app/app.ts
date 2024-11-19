8156029dc92293cf5213be59b892772c4b0c3066
Indra Zulfi Mushoddaq <me@indrazm.com>
2024-11-20 00:06:46 +0700

    updates

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,23 +6,7 @@ import type { Configuration } from "../types/config";
 import type { Route } from "../types/route";
 import { Lito } from "./lito";
 
-/**
- * Initializes the application with the given configuration and returns an object with a start function and a list of apps.
- *
- * @param {Configuration} configuration - The configuration object for the application.
- * @returns {Promise<{ start: () => void; apps: string[] }>} A promise that resolves to an object containing:
- * - `start`: A function to start the server.
- * - `apps`: An array of app names.
- *
- * The function performs the following steps:
- * 1. Sets the server port from the environment variable `PORT` or defaults to 8000.
- * 2. Initializes an empty array of routes.
- * 3. Iterates over the apps in the configuration, imports their routes, and adds them to the routes array.
- * 4. Imports public routes.
- * 5. Checks for route conflicts and logs a warning if any conflicts are found.
- * 6. Returns an object with a `start` function to start the server and the list of apps.
- */
-export async function App(configuration: Configuration): Promise<{ start: () => void; apps: string[] }> {
+export async function App(configuration: Configuration) {
     const port = Number(process.env.PORT) || 8000;
     const routes: Route[] = [];
 
@@ -43,12 +27,18 @@ export async function App(configuration: Configuration): Promise<{ start: () => 
         Logger.warn(conflictMessage);
     }
 
-    return {
-        start() {
+    const appInstance = {
+        start: () => {
             new Lito(configuration).use(routes).listen(port, () => {
                 Logger.success(`Server running on port: ${port}`);
             });
         },
         apps: configuration.apps,
+        db: configuration.db,
+        dbConfig: configuration.dbConfig,
     };
+
+    appInstance.start();
+
+    return appInstance;
 }
--- src/types/config.ts
+++ src/types/config.ts
@@ -1,3 +1,23 @@
-export interface Configuration {
+interface BaseConfig {
     apps: string[];
 }
+
+interface PostgresConfig extends BaseConfig {
+    db: "postgres";
+    dbConfig: {
+        host: string;
+        port: number;
+        username: string;
+        password: string;
+        database: string;
+    };
+}
+
+interface SqliteConfig extends BaseConfig {
+    db: "sqlite";
+    dbConfig: {
+        uri: string;
+    };
+}
+
+export type Configuration = PostgresConfig | SqliteConfig;
